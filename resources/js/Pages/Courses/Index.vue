bd43f260016a562187a6f16bb2f70d09a653fa2d
Vincent Guegan <gueganvincent@gmail.com>
2020-10-08 17:50:45 +0200

    project initialization, creation of db with user, course and episode tables and fulfilling the db with faker

---
+++ resources/js/Pages/Courses/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <app-layout>
+        <h1>Coucou</h1>
+    </app-layout>
+</template>
+
+<script>
+
+import AppLayout from './../../Layouts/AppLayout';
+
+export default {
+    components: {
+        AppLayout,
+    }
+}
+</script>
